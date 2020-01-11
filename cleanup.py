977de50a58017f01cb288757076783b77169967c
jsl12 <32917998+jsl12@users.noreply.github.com>
2020-01-11 14:23:51 -0600

    rewrote everything to use generators to be able to handle huge filesets

--- cleanup.py
+++ cleanup.py
@@ -1,91 +1,68 @@
-import datetime
 import shutil
 from pathlib import Path
-from typing import List
 
-from exif import Image
+import utils
 
-
-def copy_and_sort(
-        source_path,
-        dest_path,
-        collect_func = None,
-        sort_func = None):
-
-    source_path = Path(source_path)
-    assert source_path.exists()
-
-    dest_path = Path(dest_path)
-    if not dest_path.exists():
-        dest_path.mkdir(parents=True)
-
-    # gather all files into a single list
-    collect_func = collect_func or gather_jpg
-    files = collect_func(source_path)
-
-    # generate destination paths
-    # sort_func is a Tuple (function_pointer, arg1, arg2, arg3...)
-    sort_func = sort_func or (gen_date_paths, dest_path)
-    # calls the function pointer with the list of file Paths as the first argument and the rest of the Tuple as further positional arguments
-    res_paths = sort_func[0](files, *sort_func[1:])
-
-    for source, dest in zip(files, res_paths):
-        # Ensure the directory will exist
-        if not dest.parents[0].exists():
-            dest.parents[0].mkdir(parents=True)
-
-        # Check for duplicates
-        if dest.exists():
-            source_exif = read_exif(source)
-            dest_exif = read_exif(dest)
-
-            ATTRIBUTES = [
-                'image_unique_id',
+ATTRIBUTES = [
+                # 'image_unique_id',
                 'make',
                 'model',
                 'image_height',
                 'image_width'
             ]
-            try:
-                if all([getattr(source_exif, att) == getattr(dest_exif, att) for att in ATTRIBUTES]):
-                    print(f'Skipping duplicate {dest.name}')
+
+
+def copy_and_sort(source, dest_parent, ext='jpg'):
+    file_gen = Path(source).glob(f'**\*.{ext}')
+    for source, dest in sort_gen(file_gen, dest_parent):
+        # create parent directory for new file if it doesn't exist
+        if not dest.parents[0].exists():
+            dest.parents[0].mkdir(parents=True)
+
+        # double-check that we're not about to overwrite anything
+        if not dest.exists():
+            print(f'Copying {dest.relative_to(dest_parent)}')
+            shutil.copy2(source, dest)
+
+
+def sort_gen(source_gen, dest_parent, filename_format:str = '%Y-%m-%d_%H.%M.%S.jpg'):
+    for file in source_gen:
+        print('-' * 50)
+        try:
+            exif_orig = utils.read_exif(file, quiet=False)
+            pic_date = utils.extract_date_from_exif(exif_orig)
+            res_path = dest_parent / pic_date.strftime('%Y') / pic_date.strftime('%B') / pic_date.strftime(filename_format)
+
+            if res_path.exists():
+                print(f'{res_path.name} already exists, checking for duplicates')
+                dest_exif = utils.read_exif(res_path, quiet=False)
+                try:
+                    if check_duplicates(exif_orig, dest_exif):
+                        print(f'{file.name} is duplicate of {res_path.name}')
+                        continue
+                except AttributeError:
                     continue
-            except AttributeError:
-                pass
 
-        # Ensure the filename will be unique
-        dest = get_unique_filename(dest)
+            res_path = utils.get_unique_filename(res_path)
+            print(f'New file: {res_path.relative_to(dest_parent)}')
+            yield file, res_path
+        except utils.ExifException as e:
+            print(f'Problem reading exif data from {e}, skipping')
+            continue
 
-        # Copy file with as much metadata from the file system as possible, should preserve last modified time, etc.
-        # https://docs.python.org/3/library/shutil.html#shutil.copy2
-        print(f'Copying {source.name}')
-        shutil.copy2(source, dest)
 
-def gather_jpg(source_path: Path) -> List[Path]:
-    return [f for f in source_path.glob('**\*.jpg')]
-
-def gen_date_paths(files: List[Path], dest_parent: Path, filename_format:str = '%Y-%m-%d_%H.%M.%S.jpg') -> List[Path]:
-    print(f'Reading exif data from {len(files)} files...')
-    dates = [read_exif_date(f) for f in files]
-    res = [dest_parent / d.strftime('%Y') / d.strftime('%B') / d.strftime(filename_format) for d in dates]
-    return res
-
-def read_exif_date(path: Path) -> datetime.datetime:
-    exif_data = read_exif(path)
-    if hasattr(exif_data, 'datetime_original'):
-        date_str = exif_data.datetime_original
-    elif hasattr(exif_data, 'datetime'):
-        date_str = exif_data.datetime
-    return datetime.datetime.strptime(date_str, '%Y:%m:%d %H:%M:%S')
-
-def read_exif(path: Path) -> Image:
-    print(f'Reading exif data from {path.name}')
-    with path.open('rb') as file:
-        return Image(file)
-
-def get_unique_filename(path: Path) -> Path:
-    if path.exists():
-        files = [f for f in path.parents[0].glob(f'{path.stem}*')]
-        return path.with_name(f'{path.stem}({len(files)}){path.suffix}')
+def check_duplicates(exif_data1, exif_data2):
+    # if both sets of data have the image_unique_id field, that's pretty easy to use
+    unique_tag = 'image_unique_id'
+    if hasattr(exif_data1, unique_tag) and hasattr(exif_data2, unique_tag):
+        return getattr(exif_data1, unique_tag) == getattr(exif_data2, unique_tag)
+    # otherwise try to match all the attributes in ATTRIBUTES
     else:
-        return path
+        return all([getattr(exif_data1, att) == getattr(exif_data2, att) for att in ATTRIBUTES])
+
+
+if __name__ == '__main__':
+    # p = Path(r'M:\ARCHIVE\Pictures\Backups\2015-11-29 13.52.28\DCIM\Camera')
+    p = Path(r'M:\ARCHIVE\Pictures\Backups\2015-11-29 13.06.32')
+    dest = Path('M:\ARCHIVE\Pictures\_cleaned')
+    copy_and_sort(p, dest, 'jpg')
---
+++ utils.py
@@ -0,0 +1,34 @@
+from exif import Image
+from datetime import datetime
+
+
+class ExifException(Exception):
+    pass
+
+
+def read_exif(path, quiet=True) -> Image:
+    if not quiet:
+        print(f'Reading exif data from {path.name}')
+    with path.open('rb') as file:
+        try:
+            return Image(file)
+        except:
+            raise ExifException(f'{path.name}')
+
+
+def extract_date_from_exif(exif_data: Image) -> datetime:
+    if hasattr(exif_data, 'datetime_original'):
+        date_str = exif_data.datetime_original
+    elif hasattr(exif_data, 'datetime'):
+        date_str = exif_data.datetime
+    else:
+        raise AttributeError(f'Could not determine date from exif data')
+    return datetime.strptime(date_str, '%Y:%m:%d %H:%M:%S')
+
+
+def get_unique_filename(path: Path) -> Path:
+    if path.exists():
+        files = [f for f in path.parents[0].glob(f'{path.stem}*')]
+        return path.with_name(f'{path.stem}({len(files)}){path.suffix}')
+    else:
+        return path
