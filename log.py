56ab61d026de34366b11e0f14fc17383f6a59961
jsl12 <32917998+jsl12@users.noreply.github.com>
2020-01-12 16:38:03 -0600

    added the ability to filter with a list of strings

--- log.py
+++ log.py
@@ -2,9 +2,15 @@ from pathlib import Path
 
 
 def filter(line_gen, filter_str):
-    for line in line_gen:
-        if filter_str in line:
-            yield line
+    if isinstance(filter_str, str):
+        filter_str = [filter_str]
+
+    if isinstance(filter_str, list) and all([isinstance(s, str) for s in filter_str]):
+        for line in line_gen:
+            if all([s in line for s in filter_str]):
+                yield line
+    else:
+        raise ValueError(f'Invalid filter: {filter_str}')
 
 
 def line_gen(logfile):
