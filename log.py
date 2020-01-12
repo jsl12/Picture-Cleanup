9b50eb8f57d2f07d76d8324b20b235e04cd3aa6c
jsl12 <32917998+jsl12@users.noreply.github.com>
2020-01-12 17:09:18 -0600

    added a function to return the paths of copied files from the log file

--- log.py
+++ log.py
@@ -1,4 +1,11 @@
 from pathlib import Path
+import re
+
+
+def copied_files(logfile):
+    PATH_REGEX = re.compile('"(.+?)"')
+    for line in filter(line_gen(logfile), 'end copy'):
+        yield tuple([Path(path_str.group(1)) for path_str in PATH_REGEX.finditer(line)])
 
 
 def filter(line_gen, filter_str):
