8d5b4b1bd69272a838efa6e7a4fa2fd283a11703
jsl12 <32917998+jsl12@users.noreply.github.com>
2020-01-14 19:11:02 -0600

    added the ability to create a DataFrame from all the os level stats

--- table.py
+++ table.py
@@ -1,6 +1,10 @@
+import os
+from pathlib import Path
+
 import pandas as pd
 
 import log
+import pic_collections
 
 
 def df_copied(logfile):
@@ -27,6 +31,25 @@ def csv_copied(logfile, csv_path):
     df = df_copied(logfile)
     df.to_csv(csv_path, index=False)
     return df
+
+
+def df_from_dir(source):
+    files = [f for f in pic_collections.file_gen(source, ext=None)]
+    return pd.DataFrame(data={'path': files})
+
+
+def df_from_glob(source):
+    return pd.DataFrame(data={'path': [f for f in source]})
+
+
+def add_stats(df):
+    stat_df = pd.DataFrame(df['path'].apply(lambda p: extract_stats(os.stat(p))).to_list())
+    return pd.concat([df, stat_df], axis=1)
+
+
+def extract_stats(stat_obj):
+    res = {key: getattr(stat_obj, key) for key in dir(stat_obj) if key[:3] == 'st_'}
+    return res
 
 
 if __name__ == '__main__':
