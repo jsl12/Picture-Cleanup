db5bb9a8122d12e74bf1aaa980ce090c15228432
jsl12 <32917998+jsl12@users.noreply.github.com>
2020-01-15 12:15:53 -0600

    stat_df improvements

--- table.py
+++ table.py
@@ -1,10 +1,13 @@
-import os
+import logging
+from datetime import datetime
 from pathlib import Path
 
 import pandas as pd
 
 import log
 import utils
+
+LOGGER = logging.getLogger(__name__)
 
 
 def df_copied(logfile):
@@ -37,26 +40,30 @@ def df_from_dir_texts(source):
     return stat_df(utils.paths_from_dir_txt(source, ext=None))
 
 
-def stat_df(source, hash_keys=None):
+def stat_df(source, hash_keys=None, process_dates=True):
     files = [f for f in source]
     df = pd.concat([
         pd.DataFrame(data={'path': files, 'filename': [f.name for f in files]}),
-        pd.DataFrame([extract_stats(os.stat(f)) for f in files])
+        pd.DataFrame([extract_stats(f) for f in files])
     ], axis=1)
 
     hash_keys = hash_keys or ['filename', 'st_size']
-    if hash_keys is not None:
-        df.index = df.apply(lambda row: utils.hash([row[key] for key in hash_keys]), axis=1)
+    df.index = df.apply(lambda row: utils.hash([row[key] for key in hash_keys]), axis=1)
+
+    if process_dates:
+        for col in df:
+            if ('time' in col) and ('_ns' not in col):
+                df[col] = pd.to_datetime(df[col].apply(datetime.fromtimestamp))
     return df
 
 
-def extract_stats(stat_obj):
+def extract_stats(path: Path):
+    stat_obj = Path(path).stat()
+    LOGGER.debug(f'getting os stats: "{path}"')
     return {key: getattr(stat_obj, key) for key in dir(stat_obj) if key[:3] == 'st_'}
 
 
 if __name__ == '__main__':
-    # df = df_from_dir('PhotoCollections')
-    gen = Path('PhotoCollections').glob('*')
     gen = Path('temp').glob('**\*.jpg')
     df = stat_df(gen)
     print(df.head())
