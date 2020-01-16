3ac714a75dd3b70926f8ced8d5e68f2cc563efc2
jsl12 <32917998+jsl12@users.noreply.github.com>
2020-01-15 20:26:33 -0600

    now hashing at the end of stat_df

--- table.py
+++ table.py
@@ -56,23 +56,14 @@ def stat_df(source, hash_keys=None, parse_pathdate=True, ext='all', exclude_fold
         pd.DataFrame([extract_stats(f) for f in files])
     ], axis=1)
 
-    LOGGER.info(f'hashing indices: {df.shape[0]} files')
-    hash_keys = hash_keys or ['filename', 'st_size']
-    df.index = pd.Index(
-        data=df.apply(lambda row: utils.hash([row[key] for key in hash_keys]), axis=1),
-        name='hash'
-    )
-
     LOGGER.info(f'converting timestamps: {df.shape[0]} files')
     for col in df:
         if ('time' in col) and ('_ns' not in col):
             df[col] = pd.to_datetime(df[col].apply(datetime.fromtimestamp))
 
-
     if parse_pathdate:
         LOGGER.info(f'parsing pathdates: {df.shape[0]} files')
         df['pathdate'] = df['path'].apply(utils.scan_date)
-
 
     if ext != 'all':
         assert all([isinstance(e, str) for e in ext])
@@ -80,12 +71,18 @@ def stat_df(source, hash_keys=None, parse_pathdate=True, ext='all', exclude_fold
         ext_mask = pd.DataFrame(data={e: df['path'].apply(lambda p: p.suffix) == e for e in ext}).any(axis=1)
         df = df[ext_mask]
 
-
     if exclude_folders is not None:
         assert all([isinstance(folder, str) for folder in exclude_folders])
         LOGGER.info(f'folder exclusions: {exclude_folders}')
         exc_mask = pd.DataFrame(data={folder: df['path'].apply(lambda p: folder in str(p)) for folder in exclude_folders}).any(axis=1)
         df = df[~exc_mask]
+
+    LOGGER.info(f'hashing indices: {df.shape[0]} files')
+    hash_keys = hash_keys or ['filename', 'st_size']
+    df.index = pd.Index(
+        data=df.apply(lambda row: utils.hash([row[key] for key in hash_keys]), axis=1),
+        name='hash'
+    )
 
     return df
 
