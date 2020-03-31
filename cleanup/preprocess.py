a2f587aa28b509ce32722fe0a39527128e317e13
jsl12 <32917998+jsl12@users.noreply.github.com>
2020-03-31 18:25:01 -0500

    added some logging

--- cleanup/preprocess.py
+++ cleanup/preprocess.py
@@ -12,8 +12,11 @@ logger = logging.getLogger(__name__)
 
 
 class PreProcessor:
+    width: int = 50
+
     def process(self, df: pd.DataFrame) -> pd.DataFrame:
         raise NotImplementedError
+
 
 @dataclass
 class PreProcessFramework:
@@ -39,33 +42,46 @@ class PreProcessFramework:
         return PreProcessFramework(pre_processors=objs)
 
     def process_all(self, df: pd.DataFrame) -> pd.DataFrame:
+        logger.info(f'Beginning pre-processing of {df.shape[0]} files')
         for p in self.pre_processors:
+            logger.info(type(p))
             df = p.process(df)
+        logger.info('-' * 70)
+        logger.info(f'Total remaining files'.ljust(50) + f'{df.shape[0]}')
         return df
 
 
 @dataclass
 class FolderExcluder(PreProcessor):
     folders: List[str]
+    path_col: str = 'path'
 
-    def process(self, df: pd.DataFrame, path_col='path') -> pd.DataFrame:
-        return df[~utils.filter_path(df, self.folders, path_col)]
+    def process(self, df: pd.DataFrame) -> pd.DataFrame:
+        m = utils.filter_path(df, self.folders, self.path_col)
+        logger.info(f'Excluded files based on their paths'.ljust(self.width) + f'{m.sum()}')
+        return df[~m]
 
 
 @dataclass
 class FileIncluder(PreProcessor):
     file_types: List[str]
+    path_col: str = 'path'
 
-    def process(self, df: pd.DataFrame, path_col='path') -> pd.DataFrame:
-        return df[utils.filter_extension(df, self.file_types, path_col)]
+    def process(self, df: pd.DataFrame) -> pd.DataFrame:
+        m = utils.filter_extension(df, self.file_types, self.path_col)
+        logger.info(f'Included files based on ext'.ljust(self.width) + f'{m.sum()}')
+        return df[m]
 
 
 @dataclass
 class MinFileSize(PreProcessor):
     min_size: int
+    size_col: str = 'st_size'
 
-    def process(self, df: pd.DataFrame, size_col='st_size') -> pd.DataFrame:
-        return df[df[size_col] > self.min_size]
+    def process(self, df: pd.DataFrame, ) -> pd.DataFrame:
+        m = df[self.size_col] > self.min_size
+        logger.info(f'Above filesize limit'.ljust(self.width) + f'{m.sum()}')
+        return df[m]
 
 
 class OriginalFinder(PreProcessor):
