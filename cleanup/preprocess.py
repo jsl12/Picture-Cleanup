bf97c0700639aa8f1c4977a14be028337c22c268
jsl12 <32917998+jsl12@users.noreply.github.com>
2020-04-01 12:47:17 -0500

    pretty good base name generation, should cover most everything

--- cleanup/preprocess.py
+++ cleanup/preprocess.py
@@ -83,30 +83,34 @@ class MinFileSize(PreProcessor):
         logger.info(f'Above filesize limit'.ljust(self.width) + f'{m.sum()}')
         return df[m]
 
-
+@dataclass
 class BaseFilenameMaker(PreProcessor):
     path_col:str = 'path'
     base_col:str = 'base'
+    match_col:str = 'match'
+
+    def __post_init__(self):
+        self.regexes = [
+            re.compile('(?P<trim>_ORG)'),
+            re.compile('(?P<trim>[abc]? ?(\(\d+\))$)'),
+            re.compile('(?P<key>^(PANO|R001|_SC|MVI|CIM|VID|ST\w))(?(key).*)(?P<trim>[-_~]\d{1,3}$)'),
+            re.compile('(?P<key>^(DSC|IMG))(?(key).*)(?P<trim>[-_~]\d{1}$)'),
+            re.compile('(?P<trim>~\d+)$')
+        ]
 
     def process(self, df: pd.DataFrame) -> pd.DataFrame:
-        df[self.base_col] = df[self.path_col].apply(self.convert_base_filename)
+        cols = [self.base_col, self.match_col]
+        vals = df[self.path_col].apply(self.convert_base_filename, regexes=self.regexes)
+        df[cols] = pd.DataFrame(data=vals.to_list(), index=df.index)
         return df
 
     @staticmethod
-    def convert_base_filename(path: Path) -> str:
-        filename = path.stem.replace('_ORG', '')
-
-        if filename[:3] == 'IMG':
-            if filename[3] == '_':
-                try:
-                    datetime.strptime(filename[4:12], '%Y%m%d')
-                except Exception as e:
-                    res = filename[:8]
-                else:
-                    res = filename
-            else:
-                res = filename
-        # elif filename[:3] == 'DSC':
-        else:
-            res = filename
-        return res
+    def convert_base_filename(path: Path, regexes) -> str:
+        filename = path.stem
+        trim = ''
+        for rgx in regexes:
+            m = rgx.search(filename)
+            if m is not None:
+                filename = filename[:m.start('trim')] + filename[m.end('trim'):]
+                trim += m.group('trim')
+        return filename, trim
