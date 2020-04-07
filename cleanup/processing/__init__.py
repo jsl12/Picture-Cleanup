aaf933b9af5809177cc3b2a3839f84562efd55bd
jsl12 <32917998+jsl12@users.noreply.github.com>
2020-04-07 13:50:41 -0500

    import UniqueIDer at processing module level

--- cleanup/processing/__init__.py
+++ cleanup/processing/__init__.py
@@ -2,3 +2,4 @@ from .base import BaseFilenameMaker
 from .basic import FolderExcluder, FileIncluder, MinFileSize, ParentCol
 from .date import ScanPathDate, DateSelector
 from .processor import ConvertIfdTag
+from .unique import UniqueIDer
